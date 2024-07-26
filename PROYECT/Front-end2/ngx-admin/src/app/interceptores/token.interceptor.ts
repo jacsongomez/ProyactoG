0dde71619357aefe7398d21a3e5c28b677e01e92
jacsongomez <57466098+jacsongomez@users.noreply.github.com>
2024-07-26 00:06:03 -0500

    v3. se arregla login falta users

--- PROYECT/Front-end2/ngx-admin/src/app/interceptores/token.interceptor.ts
+++ PROYECT/Front-end2/ngx-admin/src/app/interceptores/token.interceptor.ts
@@ -28,7 +28,7 @@ export class TokenInterceptor implements HttpInterceptor {
     return next.handle(request).pipe(
       catchError((err: HttpErrorResponse) => {
         if (err.status === 401) {
-          this.router.navigateByUrl("/pages/seguridad/login");
+          this.router.navigateByUrl("/auth/loginc");
         }
         return throwError(err);
       })
